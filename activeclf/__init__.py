0a280ae9c526046ff48f01cdb05d135f03627771
AGardinon <andreagardin92@gmail.com>
2024-06-07 13:21:47 +0200

    updated the version

--- activeclf/__init__.py
+++ activeclf/__init__.py
@@ -4,7 +4,7 @@ Active Learning CLassiFier
 
 """
 
-__version__ = "1.0.5"
+__version__ = "1.0.9"
 __author__ = "Andrea Gardin"
 
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='activeclf',
-    version='1.0.8',
+    version='1.0.9',
     packages=find_packages(),
     url='https://github.com/AGardinon/ActiveLearningCLassiFier',
     author='Andrea Gardin',
