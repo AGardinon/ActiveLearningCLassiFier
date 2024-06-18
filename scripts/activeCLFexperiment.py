d395b97654a3235ade94fb41d08e02b2dbed4761
AGardinon <andreagardin92@gmail.com>
2024-06-18 11:45:02 +0200

    updated the alclf script for testing performances

--- scripts/activeCLFexperiment.py
+++ scripts/activeCLFexperiment.py
@@ -7,19 +7,46 @@
 
 import yaml
 import argparse
+import scipy
+import joblib
+import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import log_loss, accuracy_score
 
 import activeclf as alclf
 from activeclf.learning import active_learning_cycle, get_starting_batch
+from activeclf.utils.beauty import get_axes
 
 # --- func
 
+def recover_entropy(pdf: np.ndarray, decimals: int=2):
+    try:
+        entropy = np.around(scipy.stats.entropy(pdf, axis=1), decimals=decimals)
+    except:
+        entropy = np.around(scipy.stats.entropy(pdf.reshape(-1,1), axis=1), decimals=decimals)
+    return entropy
+
+def plot_line(x, y, axis):
+    axis.plot(x, y, zorder=1)
+    axis.scatter(x, y, c='0.', s=10, zorder=2)
+    axis.grid(ls='--', alpha=.5)
+    return axis
+
+
+
 # --- main
 
-def main(argument):
+def main(arguments):
 
     # read the config.yaml file provided
-    exp_config = yaml.load(open(argument.config, 'r'), Loader=yaml.FullLoader)
+    exp_config = yaml.load(open(arguments.config, 'r'), Loader=yaml.FullLoader)
+
+    # init output objects
+    fileout_name = exp_config['experimentID']
+    cross_entropy_list = list()
+    accuracy_list = list()
 
     data = alclf.DataLoader(file_path=exp_config['dataset'],
                             target=exp_config['targetVariable'])
@@ -41,7 +68,7 @@ def main(argument):
 
     # - start the ACLF experiment
     new_idxs = list()
-    for cycle in tqdm(range(exp_config['Ncycles']), desc='Cycles'):
+    for cycle in tqdm(range(exp_config['Ncycles']+1), desc='Cycles'):
 
         idxs = idxs + new_idxs
 
@@ -57,7 +84,7 @@ def main(argument):
                                                   decimals=exp_config['entropyDecimals'],
                                                   seed=None)
 
-        new_idxs = active_learning_cycle(
+        new_idxs, pdf = active_learning_cycle(
             feature_space=(data.X, data.y),
             idxs=idxs,
             new_batch=exp_config['newBatch'],
@@ -66,10 +93,27 @@ def main(argument):
             screeningSelection=exp_config['screeningSelection']
             )
 
+        predicted_labels = classifier_func.clf.predict(X=data.X)
+        accuracy_list.append(accuracy_score(y_true=data.y.to_numpy(dtype=int), y_pred=predicted_labels))
+
+        if arguments.saveout:
+            np.savetxt(fileout_name+f'_cycle{cycle}_idxs.idxs', idxs)
+            np.savetxt(fileout_name+f'_cycle{cycle}_new_idxs.idxs', new_idxs)
+            np.savetxt(fileout_name+f'_cycle{cycle}_pdf.pdf', pdf)
+            joblib.dump(classifier_func.clf, fileout_name+f'_cycle{cycle}_classifier.pkl')
+
+    if arguments.plots:
+        fig, ax = get_axes(1,1)
+        _ = plot_line(x=np.arange(exp_config['Ncycles']+1), y=accuracy_list, axis=ax)
+        ax.set_title('Accuracy score')
+        plt.show()
+
     print('\n# END')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', dest='config', required=True, type=str, help='Cycle configuration (.yaml file).')
+    parser.add_argument('-saveout', dest='saveout', required=False, action='store_true', help='To save output files.')
+    parser.add_argument('-plots', dest='plots', required=False, action='store_true', help='To plot output quantities with cycles.')
     args = parser.parse_args()
-    main(argument=args)
+    main(arguments=args)
