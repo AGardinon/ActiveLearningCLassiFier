4123b09886b75842ffd21b366686f169bfe20f6f
AGardinon <andreagardin92@gmail.com>
2024-08-09 15:51:12 +0200

    added KernelFactory for assembling kernels

--- activeclf/classification.py
+++ activeclf/classification.py
@@ -12,7 +12,6 @@ from typing import List
 
 from sklearn.svm import SVC
 from sklearn.gaussian_process import GaussianProcessClassifier
-# from sklearn.gaussian_process.kernels import RBF, Matern, ConstantKernel as C
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
@@ -117,3 +116,79 @@ def multivariate_gaussian(x: np.ndarray,
     x_m = x - mean
     return np.exp(-0.5 * np.dot(x_m, np.linalg.solve(cov, x_m))) / \
            np.sqrt((2 * np.pi) ** k * np.linalg.det(cov))
+
+
+
+from sklearn.gaussian_process.kernels import ConstantKernel, WhiteKernel
+from sklearn.gaussian_process.kernels import RBF, Matern, RationalQuadratic
+
+'''
+KernelFactoru usage.
+
+eample 1:
+classifier_dict = {
+    'kernel': ['*', {'type': 'C', 'constant_value': 1.0}, {'type': 'RBF', 'length_scale': 1.0}],
+    'n_restarts_optimizer': 5,
+    'max_iter_predict': 150,
+    'n_jobs': 3
+}
+
+kernel_factory = KernelFactory(kernel_dict=classifier_dict['kernel'])
+kernel = kernel_factory.get_kernel()
+
+-> kernel = 1**2 * RBF(length_scale=1)
+
+classifier_dict = {
+    'kernel': ['+', {'type': 'C', 'constant_value': 2.0}, ['*', 'RBF', {'type': 'W', 'noise_level': 1.0}]],
+    'n_restarts_optimizer': 5,
+    'max_iter_predict': 150,
+    'n_jobs': 3
+}
+
+kernel_factory = KernelFactory(kernel_dict=classifier_dict['kernel'])
+kernel = kernel_factory.get_kernel()
+
+-> kernel = 1.41**2 + RBF(length_scale=1) * WhiteKernel(noise_level=1)
+
+'''
+
+class KernelFactory:
+    def __init__(self, kernel_dict: dict):
+        self.kernel_dict = kernel_dict
+
+    def get_kernel(self):
+        kernel_map = {
+            'RBF': RBF,
+            'Matern': Matern,
+            'RationalQuadratic': RationalQuadratic,
+            'C': ConstantKernel,
+            'W': WhiteKernel,
+        }
+        
+        return self._parse_kernel(self.kernel_dict, kernel_map)
+    
+    def _parse_kernel(self, kernel_dict, kernel_map):
+        # Simple string case
+        if isinstance(kernel_dict, str):
+            return kernel_map[kernel_dict]()
+
+        # Dictionary with parameters
+        elif isinstance(kernel_dict, dict):
+            kernel_type = kernel_dict.pop('type')
+            return kernel_map[kernel_type](**kernel_dict)
+
+        # Composite kernel
+        elif isinstance(kernel_dict, list):
+            operator = kernel_dict[0]
+            first_kernel = self._parse_kernel(kernel_dict[1], kernel_map)
+            second_kernel = self._parse_kernel(kernel_dict[2], kernel_map)
+            
+            if operator == '*':
+                return first_kernel * second_kernel
+            elif operator == '+':
+                return first_kernel + second_kernel
+            else:
+                raise ValueError(f"Unknown operator: {operator}")
+
+        else:
+            raise ValueError(f"Invalid kernel definition: {kernel_dict}")
